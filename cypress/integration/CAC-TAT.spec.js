ac98c996f3daf017b814b32ad1c8b814828e55d4
gabriellamaas <gabriellamaas26@gmail.com>
2024-03-05 01:57:10 -0300

    alguns exercícios

---
+++ cypress/integration/CAC-TAT.spec.js
@@ -0,0 +1,48 @@
+/// <reference types="Cypress" />
+
+describe('Central de Atendimento ao Cliente TAT', function() {
+    beforeEach(function(){
+        cy.visit('./src/index.html')
+    })
+    it('verifica o título da aplicação', function() {
+        cy.visit('./src/index.html')
+        cy.title().should('be.equal','Central de Atendimento ao Cliente TAT')
+    })
+
+    it('preenche os campos obrigatórios e envia o formulário', function(){
+        const longText = 'teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste'
+        cy.get('#firstName').type('Gabriella')
+        cy.get('#lastName').type('Maas')
+        cy.get('#email').type('Gabriella@teste.com')
+        cy.get('#open-text-area').type(longText, {delay:0})
+        cy.get('button[type="submit"]').click()
+        cy.get('.success').should('be.visible')
+    })
+
+    it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', function(){
+        cy.get('#firstName').type('Gabriella')
+        cy.get('#lastName').type('Maas')
+        cy.get('#email').type('Gabriella@teste,com')
+        cy.get('#open-text-area').type('teste')
+        cy.get('button[type="submit"]').click()
+        cy.get('.error').should('be.visible')
+    })
+
+    it('campo telefone continua vazio quando preenchido com valor não-numérico', function(){
+        cy.get('#phone')
+        .type('abcdefghij')
+        .should('have.value', '')
+    })
+
+    it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', function(){
+        cy.get('#firstName').type('Gabriella')
+        cy.get('#lastName').type('Maas')
+        cy.get('#email').type('Gabriella@teste.com')
+        cy.get('#phone-checkbox').click()
+        cy.get('#open-text-area').type('teste')
+        cy.get('button[type="submit"]').click()
+        cy.get('.error').should('be.visible')
+    })
+
+  })
+  

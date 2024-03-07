dd5b68d34f8ac8eb10e900e0c4dca8685218a11a
gabriellamaas <gabriellamaas26@gmail.com>
2024-03-07 01:47:27 -0300

    alguns estudos e exercícios

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -15,7 +15,7 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.get('#lastName').type('Maas')
         cy.get('#email').type('Gabriella@teste.com')
         cy.get('#open-text-area').type(longText, {delay:0})
-        cy.get('button[type="submit"]').click()
+        cy.contains('button', 'Enviar').click()
         cy.get('.success').should('be.visible')
     })
 
@@ -24,7 +24,7 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.get('#lastName').type('Maas')
         cy.get('#email').type('Gabriella@teste,com')
         cy.get('#open-text-area').type('teste')
-        cy.get('button[type="submit"]').click()
+        cy.contains('button', 'Enviar').click()
         cy.get('.error').should('be.visible')
     })
 
@@ -38,11 +38,95 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.get('#firstName').type('Gabriella')
         cy.get('#lastName').type('Maas')
         cy.get('#email').type('Gabriella@teste.com')
-        cy.get('#phone-checkbox').click()
+        cy.get('#phone-checkbox').check()
         cy.get('#open-text-area').type('teste')
-        cy.get('button[type="submit"]').click()
+        cy.contains('button', 'Enviar').click()
         cy.get('.error').should('be.visible')
     })
 
+    it('preenche e limpa os campos nome, sobrenome, email e telefone', function(){
+        cy.get('#firstName')
+        .type('Gabriella')
+        .should('have.value', 'Gabriella')
+        .clear()
+        .should('have.value', '')
+        cy.get('#lastName')
+        .type('Maas')
+        .should('have.value', 'Maas')
+        .clear()
+        .should('have.value', '')
+        cy.get('#email')
+        .type('Gabriella@teste.com')
+        .should('have.value', 'Gabriella@teste.com')
+        .clear()
+        .should('have.value', '')
+        cy.get('#phone')
+        .type('12345678')
+        .should('have.value', '12345678')
+        .clear()
+        .should('have.value', '')
+    })
+
+    it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', function(){
+        cy.contains('button', 'Enviar').click()
+        cy.get('.error').should('be.visible')
+    })
+
+    it('envia o formulário com sucesso usando um comando customizado', function(){
+        cy.fillMandatoryFieldsAndSubmit()
+        cy.get('.success').should('be.visible')
+    })
+
+    it('seleciona um produto (YouTube) por seu texto', function(){
+        cy.get('#product')
+        .select('YouTube')
+        .should('have.value','youtube')
+    })
+
+    it('seleciona um produto (Mentoria) por seu valor (value)', function(){
+        cy.get('#product')
+        .select('mentoria')
+        .should('have.value','mentoria')
+    })
+
+    it('seleciona um produto (Blog) por seu índice', function (){
+        cy.get('#product')
+        .select(1)
+        .should('have.value','blog')
+    })
+
+    it('marca o tipo de atendimento "Feedback"', function(){
+        cy.get('input[type="radio"][value="feedback"]')
+        .check()
+        .should('have.value','feedback')
+    })
+
+    it('marca cada tipo de atendimento', function (){
+        cy.get('input[type="radio"]')
+        .should('have.length', 3)
+        .each(function($radio){
+            cy.wrap($radio).check()
+            cy.wrap($radio).should('be.checked')
+        })
+    })
+
+    it('marca ambos checkboxes, depois desmarca o último', function(){
+        cy.get('input[type="checkbox"]')
+        .check()
+        .should('be.checked')
+        .last()
+        .uncheck()
+        .should('not.be.checked')
+
+    })
+
+    it.only('seleciona um arquivo da pasta fixtures', function(){
+        cy.get('input[type="file"]')
+        .should('not.have.value')
+        .selectFile('./cypress/fixtures/example.json')
+        .should(function($input){
+            expect($input[0].files[0].name).to.equal('example.json')
+        })
+    })
   })
   
